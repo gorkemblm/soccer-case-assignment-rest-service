0339d8f5b44dadf575a22a3bfc67478e0dedc27a
Görkem Bilim <83100550+gorkemblm@users.noreply.github.com>
2021-11-08 02:44:01 +0300

    updated-footballer-service

--- src/main/java/com/gorkem/soccercase/service/FootballerService.java
+++ src/main/java/com/gorkem/soccercase/service/FootballerService.java
@@ -1,6 +1,9 @@
 package com.gorkem.soccercase.service;
 
+import com.gorkem.soccercase.exception.UserNotFoundException;
 import com.gorkem.soccercase.model.Footballer;
+import com.gorkem.soccercase.model.dto.FootballerCreateDto;
+import com.gorkem.soccercase.model.dto.FootballerUpdateDto;
 import com.gorkem.soccercase.model.dto.converters.FootballerDtoConverter;
 import com.gorkem.soccercase.model.dto.FootballerRetrieveDto;
 import com.gorkem.soccercase.repository.FootballerRepository;
@@ -24,7 +27,40 @@ public class FootballerService {
         List<Footballer> footballers = this.footballerRepository.findAll();
 
         return footballers.stream()
-                .map(footballerDtoConverter :: convertForRetrieve)
+                .map(footballerDtoConverter::convertForRetrieve)
                 .collect(Collectors.toList());
+    }
+
+    public FootballerRetrieveDto createFootballer(FootballerCreateDto footballerCreateDto) {
+        Footballer footballer = this.footballerDtoConverter.convertForCreate(footballerCreateDto);
+
+        return this.footballerDtoConverter.convertForRetrieve(this.footballerRepository.save(footballer));
+    }
+
+    public FootballerRetrieveDto updateFootballer(String id, FootballerUpdateDto footballerUpdateDto) {
+        Footballer footballer = this.footballerRepository.getById(id);
+
+        Footballer updatedFootballer = this.footballerDtoConverter.convertForUpdate(footballerUpdateDto, footballer);
+
+        return this.footballerDtoConverter.convertForRetrieve(this.footballerRepository.save(updatedFootballer));
+    }
+
+    public boolean deleteFootballer(String id) {
+        Footballer footballer = this.footballerRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(String.format("Footballer not found for this id : %s", id)));
+
+        try {
+            this.footballerRepository.delete(footballer);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public FootballerRetrieveDto retrieveFootballer(String id) {
+        Footballer footballer = this.footballerRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(String.format("Footballer not found for this id : %s", id)));
+
+        return this.footballerDtoConverter.convertForRetrieve(footballer);
     }
 }

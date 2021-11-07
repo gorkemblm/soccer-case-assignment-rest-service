c69752af248e3bf14b797b8aace86b10b38edeb0
Görkem Bilim <83100550+gorkemblm@users.noreply.github.com>
2021-11-07 22:27:07 +0300

    added-project

---
+++ src/main/java/com/gorkem/soccercase/SoccerCaseApplication.java
@@ -0,0 +1,13 @@
+package com.gorkem.soccercase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoccerCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gorkem/soccercase/SoccerCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gorkem.soccercase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

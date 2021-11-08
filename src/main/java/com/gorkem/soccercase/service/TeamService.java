e3c590e3032dcaff3c797ab2a379efc540ea62de
Görkem Bilim <83100550+gorkemblm@users.noreply.github.com>
2021-11-08 03:14:06 +0300

    updated-team-service

--- src/main/java/com/gorkem/soccercase/service/TeamService.java
+++ src/main/java/com/gorkem/soccercase/service/TeamService.java
@@ -1,7 +1,67 @@
 package com.gorkem.soccercase.service;
 
+import com.gorkem.soccercase.exception.ResourceNotFoundException;
+import com.gorkem.soccercase.model.Team;
+import com.gorkem.soccercase.model.dto.TeamCreateDto;
+import com.gorkem.soccercase.model.dto.TeamRetrieveDto;
+import com.gorkem.soccercase.model.dto.TeamUpdateDto;
+import com.gorkem.soccercase.model.dto.converters.TeamDtoConverter;
+import com.gorkem.soccercase.repository.TeamRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TeamService {
+
+    private final TeamRepository teamRepository;
+    private final TeamDtoConverter teamDtoConverter;
+
+    public TeamService(TeamRepository teamRepository, TeamDtoConverter teamDtoConverter) {
+        this.teamRepository = teamRepository;
+        this.teamDtoConverter = teamDtoConverter;
+    }
+
+    public List<TeamRetrieveDto> retrieveTeams() {
+        List<Team> teams = this.teamRepository.findAll();
+
+        return teams.stream()
+                .map(teamDtoConverter::convertForRetrieve)
+                .collect(Collectors.toList());
+    }
+
+    public TeamRetrieveDto createTeam(TeamCreateDto teamCreateDto) {
+        Team team = this.teamDtoConverter.convertForCreate(teamCreateDto);
+
+        return this.teamDtoConverter.convertForRetrieve(this.teamRepository.save(team));
+    }
+
+    public TeamRetrieveDto updateTeam(String id, TeamUpdateDto teamUpdateDto) {
+        Team team = this.teamRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Team not found for this id : %s", id)));
+
+        Team updatedTeam = this.teamDtoConverter.convertForUpdate(teamUpdateDto, team);
+
+        return this.teamDtoConverter.convertForRetrieve(this.teamRepository.save(updatedTeam));
+    }
+
+    public boolean deleteTeam(String id) {
+        Team team = this.teamRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Team not found for this id : %s", id)));
+
+        try {
+            this.teamRepository.delete(team);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public TeamRetrieveDto retrieveTeam(String id) {
+        Team team = this.teamRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Team not found for this id : %s", id)));
+
+        return this.teamDtoConverter.convertForRetrieve(team);
+    }
 }

cce23be68f4f4c4e8aa04f680a23d264f5e5de94
Görkem Bilim <83100550+gorkemblm@users.noreply.github.com>
2021-11-10 07:34:55 +0300

    footballer-service-refactoring
    added try-catch blocks
    throwed custom exception classes and
    code simplification done

--- src/main/java/com/gorkem/soccercase/service/FootballerService.java
+++ src/main/java/com/gorkem/soccercase/service/FootballerService.java
@@ -1,5 +1,7 @@
 package com.gorkem.soccercase.service;
 
+import com.gorkem.soccercase.constants.Message;
+import com.gorkem.soccercase.exception.ServiceLayerException;
 import com.gorkem.soccercase.exception.ResourceNotFoundException;
 import com.gorkem.soccercase.model.Footballer;
 import com.gorkem.soccercase.model.Team;
@@ -27,8 +29,13 @@ public class FootballerService {
     }
 
     public List<FootballerRetrieveDto> retrieveFootballers() {
-        List<Footballer> footballers = this.footballerRepository.findAll();
+        List<Footballer> footballers;
+        try {
+            footballers = this.footballerRepository.findAll();
 
+        } catch (Exception e) {
+            throw new ServiceLayerException(Message.FOOTBALLER_FAIL_RETRIEVE_RECORDS);
+        }
         return footballers.stream()
                 .map(footballerDtoConverter::convertForRetrieve)
                 .collect(Collectors.toList());
@@ -39,36 +46,50 @@ public class FootballerService {
 
         Footballer footballer = this.footballerDtoConverter.convertForCreate(footballerCreateDto, team);
 
-        return this.footballerDtoConverter.convertForRetrieve(this.footballerRepository.save(footballer));
+        Footballer savedFootballer;
+        try {
+            savedFootballer = this.footballerRepository.save(footballer);
+        } catch (Exception e) {
+            throw new ServiceLayerException(Message.FOOTBALLER_FAIL_SAVING_RECORD);
+        }
+        return this.footballerDtoConverter.convertForRetrieve(savedFootballer);
     }
 
     public FootballerRetrieveDto updateFootballer(String id, FootballerUpdateDto footballerUpdateDto) {
         Team team = this.teamService.findTeamById(footballerUpdateDto.getTeamId());
 
-        Footballer footballer = this.footballerRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException(String.format("Footballer not found for this id : %s", id)));
+        Footballer footballer = findFootballerById(id);
 
         Footballer updatedFootballer = this.footballerDtoConverter.convertForUpdate(footballerUpdateDto, footballer, team);
 
-        return this.footballerDtoConverter.convertForRetrieve(this.footballerRepository.save(updatedFootballer));
+        Footballer savedFootballer;
+        try {
+            savedFootballer = this.footballerRepository.save(updatedFootballer);
+
+        } catch (Exception e) {
+            throw new ServiceLayerException(Message.FOOTBALLER_FAIL_UPDATING_RECORD);
+        }
+        return this.footballerDtoConverter.convertForRetrieve(savedFootballer);
     }
 
     public boolean deleteFootballer(String id) {
-        Footballer footballer = this.footballerRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException(String.format("Footballer not found for this id : %s", id)));
-
+        Footballer footballer = findFootballerById(id);
         try {
             this.footballerRepository.delete(footballer);
-            return true;
         } catch (Exception e) {
-            return false;
+            throw new ServiceLayerException(Message.FOOTBALLER_FAIL_DELETING_RECORD);
         }
+        return true;
     }
 
     public FootballerRetrieveDto retrieveFootballer(String id) {
-        Footballer footballer = this.footballerRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException(String.format("Footballer not found for this id : %s", id)));
+        Footballer footballer = findFootballerById(id);
 
         return this.footballerDtoConverter.convertForRetrieve(footballer);
+    }
+
+    public Footballer findFootballerById(String id) {
+        return this.footballerRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(Message.PUBLIC_FAIL_RECORD_NOT_FOUND + id));
     }
 }

6275e9b75bc2a45697609b018a3ceb32231a2f0f
Görkem Bilim <gorkemblm@gmail.com>
2021-11-10 07:53:45 +0300

    Constants && conf (#14)
    * added-messages-for-code-simplify
    
    * added-stacktrace-conf-for-not-to-see

---
+++ src/main/java/com/gorkem/soccercase/constants/Message.java
@@ -0,0 +1,22 @@
+package com.gorkem.soccercase.constants;
+
+public class Message {
+    /*
+    GLOBAL MESSAGES
+     */
+    public static final String PUBLIC_FAIL_RECORD_NOT_FOUND = "Footballer not found for this id : ";
+    /*
+    FOOTBALLER BUSINESS MESSAGES
+     */
+    public static final String FOOTBALLER_FAIL_RETRIEVE_RECORDS = "There was a problem retrieving records from the footballer database.";
+    public static final String FOOTBALLER_FAIL_SAVING_RECORD = "There was a problem saving the recording to the footballer database.";
+    public static final String FOOTBALLER_FAIL_UPDATING_RECORD = "There was a problem saving the updated record to the footballer database.";
+    public static final String FOOTBALLER_FAIL_DELETING_RECORD = "An error occurred while deleting a record from the footballer database.";
+    /*
+    TEAM BUSINESS MESSAGES
+     */
+    public static final String TEAM_FAIL_RETRIEVE_RECORDS = "There was a problem retrieving records from the team database.";
+    public static final String TEAM_FAIL_SAVING_RECORD = "There was a problem saving the recording to the team database.";
+    public static final String TEAM_FAIL_UPDATING_RECORD= "There was a problem saving the updated record to the team database.";
+    public static final String TEAM_FAIL_DELETING_RECORD = "An error occurred while deleting a record from the team database.";
+}

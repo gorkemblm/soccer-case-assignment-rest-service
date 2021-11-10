c4b8a45e9b9145ee0e9cf4d8adb66242cdd72147
Görkem Bilim <83100550+gorkemblm@users.noreply.github.com>
2021-11-10 07:35:40 +0300

    footballer-repository-refactoring
    added @repository annotation for possible repository errors

--- src/main/java/com/gorkem/soccercase/repository/FootballerRepository.java
+++ src/main/java/com/gorkem/soccercase/repository/FootballerRepository.java
@@ -2,6 +2,8 @@ package com.gorkem.soccercase.repository;
 
 import com.gorkem.soccercase.model.Footballer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FootballerRepository extends JpaRepository<Footballer, String> {
 }

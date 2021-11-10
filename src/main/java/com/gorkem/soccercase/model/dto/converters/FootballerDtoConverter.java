78d4aa67ad61f2f50557a8c3ac1a84fc019ee4a3
Görkem Bilim <83100550+gorkemblm@users.noreply.github.com>
2021-11-10 04:09:58 +0300

    footballer-converter-refactoring
    added id property into in converter methods to see when we list, added multipleConverter method for  ease of use,  added update property for updating process,  changed method logics to add the team property

--- src/main/java/com/gorkem/soccercase/model/dto/converters/FootballerDtoConverter.java
+++ src/main/java/com/gorkem/soccercase/model/dto/converters/FootballerDtoConverter.java
@@ -1,41 +1,56 @@
 package com.gorkem.soccercase.model.dto.converters;
 
 import com.gorkem.soccercase.model.Footballer;
+import com.gorkem.soccercase.model.Team;
 import com.gorkem.soccercase.model.dto.FootballerCreateDto;
 import com.gorkem.soccercase.model.dto.FootballerRetrieveDto;
 import com.gorkem.soccercase.model.dto.FootballerUpdateDto;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class FootballerDtoConverter {
 
     public FootballerRetrieveDto convertForRetrieve(Footballer footballer) {
         FootballerRetrieveDto dto = new FootballerRetrieveDto();
+        dto.setId(footballer.getId());
         dto.setAge(footballer.getAge());
         dto.setFirstName(footballer.getFirstName());
         dto.setLastName(footballer.getLastName());
         dto.setNationality(footballer.getNationality());
-        dto.setTeam(footballer.getTeam());
+        dto.setTeamName(footballer.getTeam().getName());
         dto.setPosition(footballer.getPosition());
 
         return dto;
     }
 
-    public Footballer convertForCreate(FootballerCreateDto footballerCreateDto) {
+    public List<FootballerRetrieveDto> multipleConvertForRetrieve(List<Footballer> footballers) {
+
+        return footballers.stream()
+                .map(this::convertForRetrieve)
+                .collect(Collectors.toList());
+    }
+
+    public Footballer convertForCreate(FootballerCreateDto footballerCreateDto, Team team) {
         return Footballer.builder()
                 .age(footballerCreateDto.getAge())
                 .nationality(footballerCreateDto.getNationality())
                 .position(footballerCreateDto.getPosition())
-                .team(footballerCreateDto.getTeam())
+                .team(team)
                 .firstName(footballerCreateDto.getFirstName())
                 .lastName(footballerCreateDto.getLastName())
                 .build();
     }
 
-    public Footballer convertForUpdate(FootballerUpdateDto footballerUpdateDto, Footballer footballer) {
+    public Footballer convertForUpdate(FootballerUpdateDto footballerUpdateDto, Footballer footballer, Team team) {
         footballer.setAge(footballerUpdateDto.getAge());
         footballer.setPosition(footballerUpdateDto.getPosition());
-        footballer.setTeam(footballerUpdateDto.getTeam());
+        footballer.setTeam(team);
+        footballer.setUpdatedAt(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")));
 
         return footballer;
     }

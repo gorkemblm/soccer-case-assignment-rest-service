fe33022b412801b39d36dc19fe131f7d5ef0f904
Görkem Bilim <83100550+gorkemblm@users.noreply.github.com>
2021-11-10 04:15:29 +0300

    teams-controller-refactoring
    changed endpoints and mapping methods, added property filter for the properties we want to see.
    
    -DUPLICATE CODE will be resolved

--- src/main/java/com/gorkem/soccercase/controller/TeamsController.java
+++ src/main/java/com/gorkem/soccercase/controller/TeamsController.java
@@ -1,17 +1,22 @@
 package com.gorkem.soccercase.controller;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import com.gorkem.soccercase.model.dto.TeamCreateDto;
 import com.gorkem.soccercase.model.dto.TeamRetrieveDto;
 import com.gorkem.soccercase.model.dto.TeamUpdateDto;
 import com.gorkem.soccercase.service.TeamService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/teams")
 public class TeamsController {
 
     private final TeamService teamService;
@@ -20,29 +25,44 @@ public class TeamsController {
         this.teamService = teamService;
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/teams")
+    @GetMapping
     public ResponseEntity<Object> retrieveTeams() {
-        return ResponseEntity.ok(this.teamService.retrieveTeams());
+        List<TeamRetrieveDto> teamRetrieveDtos = this.teamService.retrieveTeams();
+
+        SimpleBeanPropertyFilter propertyFilterForTeams = SimpleBeanPropertyFilter
+                .filterOutAllExcept("name", "footballers");
+
+        SimpleBeanPropertyFilter propertyFilterForFootballers = SimpleBeanPropertyFilter
+                .filterOutAllExcept("firstName", "lastName", "age", "position");
+
+        FilterProvider filterProvider = new SimpleFilterProvider()
+                .addFilter("RetrieveFootballerFilter", propertyFilterForFootballers)
+                .addFilter("RetrieveTeamFilter", propertyFilterForTeams);
+
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(teamRetrieveDtos);
+        mappingJacksonValue.setFilters(filterProvider);
+
+        return ResponseEntity.ok(mappingJacksonValue);
     }
 
-    @RequestMapping(method = RequestMethod.POST, path = "/create-team")
+    @PostMapping
     public ResponseEntity<Object> createTeam(@RequestBody TeamCreateDto teamCreateDto) {
         TeamRetrieveDto savedTeam = this.teamService.createTeam(teamCreateDto);
 
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
-                .path("/{id}")
+                .path("/retrieve-team/{id}")
                 .buildAndExpand(savedTeam.getId()).toUri();
 
         return ResponseEntity.created(location).build();
     }
 
-    @RequestMapping(method = RequestMethod.PUT, path = "/update-team/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<Object> update(@PathVariable(name = "id") String id, @RequestBody TeamUpdateDto teamUpdateDto) {
         return ResponseEntity.ok(this.teamService.updateTeam(id, teamUpdateDto));
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, path = "/delete-team/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteTeam(@PathVariable(name = "id") String id) {
         var isDeletedTeam = this.teamService.deleteTeam(id);
 
@@ -53,8 +73,24 @@ public class TeamsController {
         }
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/retrieve-team/{id}")
+    //Duplicate CODE -> move property filters to global scope
+    @GetMapping("/{id}")
     public ResponseEntity<Object> retrieveTeam(@PathVariable(name = "id") String id) {
-        return ResponseEntity.ok(this.teamService.retrieveTeam(id));
+        TeamRetrieveDto teamRetrieveDto = this.teamService.retrieveTeam(id);
+
+        SimpleBeanPropertyFilter propertyFilterForTeams = SimpleBeanPropertyFilter
+                .filterOutAllExcept("name", "footballers");
+
+        SimpleBeanPropertyFilter propertyFilterForFootballers = SimpleBeanPropertyFilter
+                .filterOutAllExcept("firstName", "lastName", "age", "position");
+
+        FilterProvider filterProvider = new SimpleFilterProvider()
+                .addFilter("RetrieveFootballerFilter", propertyFilterForFootballers)
+                .addFilter("RetrieveTeamFilter", propertyFilterForTeams);
+
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(teamRetrieveDto);
+        mappingJacksonValue.setFilters(filterProvider);
+
+        return ResponseEntity.ok(mappingJacksonValue);
     }
 }
